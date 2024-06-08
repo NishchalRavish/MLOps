cd8ab5e80439253ccb2b59b94e64121a360e7eb2
Nishchal Ravish <ghanteravish@gmail.com>
2024-06-07 23:57:54 -0500

    Update_port_no

--- ml-ci-cd-jenkins/main.py
+++ ml-ci-cd-jenkins/main.py
@@ -78,4 +78,4 @@ def predict_gui(Gender: str,
     return {'status':pred}
 
 if __name__ =="__main__":
-    uvicorn.run(app, host="0.0.0.0", port=80005)
+    uvicorn.run(app, host="0.0.0.0", port=8005)
